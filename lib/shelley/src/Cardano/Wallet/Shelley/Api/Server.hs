c8082390c185340eef84bf428f601c86e1d2fa37
Pawel Jakubas <pawel.jakubas@iohk.io>
2021-03-03 20:24:47 +0100

    add scaffolding for getSharedWallet endpoint

--- lib/shelley/src/Cardano/Wallet/Shelley/Api/Server.hs
+++ lib/shelley/src/Cardano/Wallet/Shelley/Api/Server.hs
@@ -230,7 +230,7 @@ server byron icarus shelley spl ntp =
     :<|> proxy
     :<|> settingS
     :<|> smash
-    :<|> shareWallets
+    :<|> sharedWallets
   where
     wallets :: Server Wallets
     wallets = deleteWallet shelley
@@ -490,8 +490,12 @@ server byron icarus shelley spl ntp =
                 FetchSMASH smashServer -> getHealth smashServer
                 _ -> pure (ApiHealthCheck NoSmashConfigured)
 
-    shareWallets :: Server SharedWallets
-    shareWallets = pure $ throwError err501
+    sharedWallets :: Server SharedWallets
+    sharedWallets =
+        postSharedWallet :<|> getSharedWallet
+      where
+         postSharedWallet = pure $ throwError err501
+         getSharedWallet = pure $ throwError err501
 
 postAnyAddress
     :: NetworkId
